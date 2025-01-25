085d113594a801d979790c8895b3c1bc0c70ee19
itsJoaoVictor <jvgf98@gmail.com>
2025-01-25 00:09:58 -0300

    Refatora funções de partição do algoritmo Quicksort e ajusta estrutura de saída

--- joaovictorflorencio_201700100141_quicksort.c
+++ joaovictorflorencio_201700100141_quicksort.c
@@ -52,40 +52,48 @@ void quicksortLP(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
     }
 }
 
-// Função Lomuto por Mediana de 3
-int particionaLM(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
+int particionaLM(int arr[], int baixo, int alto, int *trocas, int *chamadas)
+{
     int n = alto - baixo + 1;
-    
+
     // Definir os índices V1, V2 e V3
     int V1 = baixo + n / 4;
     int V2 = baixo + n / 2;
     int V3 = baixo + 3 * n / 4;
-    
-    // Ordenar V1, V2, V3
+
+    // Ordenar V1, V2, V3 para garantir que a mediana seja o valor do meio
     if (arr[V1] > arr[V2]) troca(&arr[V1], &arr[V2]);
     if (arr[V2] > arr[V3]) troca(&arr[V2], &arr[V3]);
     if (arr[V1] > arr[V2]) troca(&arr[V1], &arr[V2]);
-    
+
     // Agora, a mediana está em V2, e será o pivô
     int pivo = arr[V2];
-    
+
     // Troca o pivô com o último elemento
     troca(&arr[V2], &arr[alto]);
     (*trocas)++;
-    
+
     int i = baixo - 1;
-    for (int j = baixo; j < alto; j++) {
-        if (arr[j] <= pivo) {
+
+    // Loop de partição
+    for (int j = baixo; j < alto; j++)
+    {
+        if (arr[j] <= pivo)
+        {
             i++;
             troca(&arr[i], &arr[j]);
             (*trocas)++;
         }
     }
+
+    // Troca o pivô de volta para a posição correta
     troca(&arr[i + 1], &arr[alto]);
     (*trocas)++;
-    
+
+    // Aqui, o pivô é agora na posição correta, e retornamos o índice do pivô
     return i + 1;
 }
+
 
 
 void quicksortLM(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
@@ -99,8 +107,14 @@ void quicksortLM(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
 
 // Função Lomuto por Pivô Aleatório
 int particionaLA(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
-    int pivoIndice = baixo + rand() % (alto - baixo + 1);
+    // Calculando o índice do pivô com a fórmula fornecida
+    int n = alto - baixo + 1;
+    int pivoIndice = baixo + abs(arr[baixo]) % n;  // Calculando com o valor absoluto de arr[baixo] e ajustando com mod n
+
+    // Troca o pivô com o último elemento
     troca(&arr[alto], &arr[pivoIndice]);
+    
+    // Chama a partição Lomuto normal
     return particionaLP(arr, baixo, alto, trocas, chamadas);
 }
 
@@ -138,54 +152,73 @@ void quicksortHP(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
 
 // Função Hoare por Mediana de 3
 int particionaHM(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
-    int meio = baixo + (alto - baixo) / 2;
+    int n = alto - baixo + 1;
 
-    // Ordenar baixo, meio e alto
-    if (arr[baixo] > arr[meio]) troca(&arr[baixo], &arr[meio]);
-    if (arr[meio] > arr[alto]) troca(&arr[meio], &arr[alto]);
-    if (arr[baixo] > arr[meio]) troca(&arr[baixo], &arr[meio]);
+    // Definir os índices V1, V2 e V3
+    int V1 = baixo + n / 4;
+    int V2 = baixo + n / 2;
+    int V3 = baixo + 3 * n / 4;
 
-    // Usar a mediana como pivô
-    int pivo = arr[meio];
+    // Ordenar V1, V2, V3 para garantir que a mediana seja o valor do meio
+    if (arr[V1] > arr[V2]) troca(&arr[V1], &arr[V2]);
+    if (arr[V2] > arr[V3]) troca(&arr[V2], &arr[V3]);
+    if (arr[V1] > arr[V2]) troca(&arr[V1], &arr[V2]);
+
+    // Agora, a mediana está em V2, e será o pivô
+    int pivo = arr[V2];
+
+    // Troca o pivô com o primeiro elemento (pois Hoare utiliza o pivô na posição inicial)
+    troca(&arr[baixo], &arr[V2]);
+    (*trocas)++;
 
     int i = baixo - 1;
     int j = alto + 1;
+
+    // Loop de partição Hoare
     while (1) {
-        do { i++; } while (arr[i] < pivo);
-        do { j--; } while (arr[j] > pivo);
-        if (i >= j) return j;
+        do { i++; } while (arr[i] < pivo);  // Move i até encontrar valor >= pivô
+        do { j--; } while (arr[j] > pivo);  // Move j até encontrar valor <= pivô
+
+        if (i >= j) return j;  // Se os índices se cruzam, partição finalizada
+
+        // Troca os elementos nos índices i e j
         troca(&arr[i], &arr[j]);
         (*trocas)++;
     }
 }
 
+// Função QuickSort com partição Hoare
 void quicksortHM(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
     (*chamadas)++;
     if (baixo < alto) {
+        // Particionar e obter o índice do pivô
         int pivo = particionaHM(arr, baixo, alto, trocas, chamadas);
-        quicksortHM(arr, baixo, pivo - 1, trocas, chamadas);
+
+        // Chamada recursiva para as duas partições
+        quicksortHM(arr, baixo, pivo, trocas, chamadas);
         quicksortHM(arr, pivo + 1, alto, trocas, chamadas);
     }
 }
 
+
 // Função Hoare por Pivô Aleatório
 int particionaHA(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
-    // Escolhe um índice aleatório para o pivô
-    int pivoIndice = baixo + rand() % (alto - baixo + 1);
-    // Troca o pivô aleatório com o último elemento
+    // Calculando o índice do pivô com a fórmula fornecida
+    int n = alto - baixo + 1;
+    int pivoIndice = baixo + abs(arr[baixo]) % n;  // Calculando com o valor absoluto de arr[baixo] e ajustando com mod n
+
+    // Troca o pivô com o último elemento
     troca(&arr[alto], &arr[pivoIndice]);
     (*trocas)++;  // Contabilizando a troca
 
-    // Agora a partição pode ser feita normalmente
+    // Chama a partição Hoare normal
     return particionaHP(arr, baixo, alto, trocas, chamadas);
 }
 
 void quicksortHA(int arr[], int baixo, int alto, int* trocas, int* chamadas) {
     (*chamadas)++;
     if (baixo < alto) {
-        // Chama a partição
         int pivo = particionaHA(arr, baixo, alto, trocas, chamadas);
-        // Recursão nos sub-arrays
         quicksortHA(arr, baixo, pivo, trocas, chamadas);
         quicksortHA(arr, pivo + 1, alto, trocas, chamadas);
     }
